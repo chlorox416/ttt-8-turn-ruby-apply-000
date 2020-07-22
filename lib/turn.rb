f50672a151403710153bfa1d9219caae2800fc69
C. Chang <chloechang416@gmail.com>
2020-07-22 01:01:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ def valid_move?(board, position)
   end
 end
   # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-=
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
